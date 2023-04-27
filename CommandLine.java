6a17bbad545ef1d64a5abc85b31ca8bd69861431
Sankeerthana1966 <102963198+Sankeerthana1966@users.noreply.github.com>
2023-04-27 19:08:04 +0530

    Add files via upload

---
+++ CommandLine.java
@@ -0,0 +1,11 @@
+class CommandLine 
+{
+	public static void main(String args[])
+	{
+		System.out.println("\n Command -line arguments are:");
+		for(String str:args)
+		{
+			System.out.println(str);
+		}
+	}
+}
